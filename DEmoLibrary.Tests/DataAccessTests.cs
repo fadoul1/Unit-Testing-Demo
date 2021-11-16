edfd7be17b9a21b8147349f209762b13e9ccbca1
fad ouro agoro <fadouro@gmail.com>
2021-11-16 09:24:54 +0000

    Add project files.

---
+++ DEmoLibrary.Tests/DataAccessTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using DemoLibrary.Models;
+
+namespace DemoLibrary.Tests
+{
+    public class DataAccessTests
+    {
+        [Fact]
+        public void AddPersonToPeopleList_ShouldWork()
+        {
+            PersonModel newPerson = new PersonModel { 
+                FirstName = "Fad",
+                LastName = "OURO" 
+            };
+
+            List<PersonModel> people = new List<PersonModel>();
+
+            DataAccess.AddPersonToPeopleList(people, newPerson);
+
+            Assert.True(people.Count == 1);
+            Assert.Contains<PersonModel>(newPerson, people);
+        }
+
+        [Theory]
+        [InlineData("Fad", "", "LastName")]
+        [InlineData("", "OURO", "FirstName")]
+        public void AddPersonToPeopleList_ShouldFail(string firstName, string lastName, string param)
+        {
+            PersonModel newPerson = new PersonModel
+            {
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            List<PersonModel> people = new List<PersonModel>();
+
+            Assert.Throws<ArgumentException>(
+                param,
+                () => DataAccess.AddPersonToPeopleList(people, newPerson));
+
+        }
+    }
+}
---
+++ DEmoLibrary.Tests/ExamplesTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class ExamplesTests
+    {
+        [Fact]
+        public void ExampleLoadTextFile_ValidNameShouldWork()
+        {
+            string actual = Examples.ExampleLoadTextFile("This is a valid name");
+
+            Assert.True(actual.Length > 0);
+        }
+
+        [Fact]
+        public void ExampleLoadTextFile_InValidNameShouldWork()
+        {
+            Assert.Throws <ArgumentException> (
+                "file",
+                () => Examples.ExampleLoadTextFile(""));
+        }
+    }
+}
